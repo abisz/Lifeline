9ff6c7c7346a93854e5bb48430b667cffe6f2cab
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-10-19 11:07:51 +0200

    Adds Label for current card

--- app/components/CurrentCard/index.js
+++ app/components/CurrentCard/index.js
@@ -1,8 +1,36 @@
 var CurrentCard = React.createClass({
+
+  getCardLabel: function () {
+
+    if ( ! store.getState().current ) return;
+
+    switch (store.getState().current) {
+      case 'one':
+        return '1';
+      case 'two':
+        return '2';
+      case 'three':
+        return '3';
+      case 'doomsday':
+        return '☠️';
+      case 'firstPlayerEvent':
+        return 'Die führende Person muss eine Runde aussetzen';
+      case 'lastPlayerEvent':
+        return 'Die letzte Person verliert eine Karte';
+      case 'doubleOrNothing':
+        return 'Jede Person kann entscheiden eine Karte zu verdoppeln/verlieren (Würfel)';
+      case 'everyOneLoses':
+        return 'Jede Person verliert die geringste Karte';
+      default:
+        return 'Unbekannte Karte';
+    }
+
+  },
+
   render: function () {
     return (
       <div>
-        <h1>{ store.getState().current }</h1>
+        <h1>{ this.getCardLabel() }</h1>
       </div>
     );
   }
